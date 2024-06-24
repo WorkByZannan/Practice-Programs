639cef59c6cae79ee04573289cd34e68418deeb3
Zannan <zannan.work@gmail.com>
2024-06-24 15:10:16 +0530

    Practice Programs

--- lib/patterns/a_pattern.dart
+++ lib/patterns/a_pattern.dart
@@ -4,11 +4,12 @@ void main(){
   print('Enter the number of rows : ');
   int rows = int.parse(stdin.readLineSync()!);
   for (int i = 1;i <= rows;i++){
-    for (int j = 1;j <= rows - i;j++){
-      stdout.write(' ');
-    }
-    for (int k = 1;k <= 2 * i - 1;k++){
-      stdout.write('*');
+    for (int j = 1;j <= rows;j++){
+      if (i == 1 || i == rows - 2 || j == 1 || j == rows){
+        stdout.write('* ');
+      }else {
+        stdout.write('  ');
+      }
     }
     stdout.write('\n');
   }
--- lib/patterns/diamond_pattern.dart
+++ lib/patterns/diamond_pattern.dart
@@ -1,24 +1,24 @@
 import 'dart:io';
-void main() {
-  int i, j, k;
+
+void main(){
   print('Enter the number of rows : ');
   int rows = int.parse(stdin.readLineSync()!);
-  for (i = 1; i <= rows - 4; i++) {
-    for (j = 1; j <= rows - 4 - i; j++) {
+  for (int i = 1;i <= rows;i++){
+    for (int j = 1;j <= rows - i;j++){
       stdout.write(' ');
     }
-    for (k = 1; k <= 2 * i - 1; k++) {
-      stdout.write('*');
+    for (int k = 2;k <= i + 1;k++){
+      stdout.write('* ');
     }
     stdout.write('\n');
   }
-    for (int i = 1; i <= rows - 5; i++) {
-      for (int j = 1; j <= 2 * i - i; j++) {
-        stdout.write(' ');
-      }
-      for (int k = 1; k <= rows - 2 * i ; k++) {
-        stdout.write('*');
-      }
-      stdout.write('\n');
+  for (int i = 1;i <= rows - 1;i++){
+    for (int j = 1;j <= 2 * i - i;j++){
+      stdout.write(' ');
+    }
+    for (int k = 1;k <= rows - i;k++){
+      stdout.write('* ');
+    }
+    stdout.write('\n');
   }
 }
